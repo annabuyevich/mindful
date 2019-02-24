21c2340a09eb4ef8868cd068a7653b8fe5e9e4e9
annabuyevich <avbuyevich@gmail.com>
2019-02-23 19:14:29 -0600

    new backend

--- my-app/src/app/camera/camera.component.ts
+++ my-app/src/app/camera/camera.component.ts
@@ -8,59 +8,60 @@ import * as $ from 'jquery';
 })
 export class CameraComponent implements OnInit {
   //    declare var require: any;
-      @ViewChild("video")
-      public video: ElementRef;
+  @ViewChild("video")
+  public video: ElementRef;
 
-      @ViewChild("canvas")
-      public canvas: ElementRef;
+  @ViewChild("canvas")
+  public canvas: ElementRef;
 
-      public captures: Array<"image">;
+  public captures: Array<"image">;
 
-      public constructor() {
-          this.captures = [];
-      }
+  public constructor() {
+    this.captures = [];
+  }
 
-      public ngOnInit() {
-
-      }
-
-      public ngAfterViewInit() {
-          if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-              navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-                  this.video.nativeElement.srcObject = stream;
-                  this.video.nativeElement.play();
-              });
-          }
-      }
-
-      public capture() {
-          var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
-          this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
-        //  console.log(this.captures[0]);
-
-          $.ajax({
-            type: "POST",
-            url:'../../assets/emotionDetection.py',
-            data: {img: this.capture[0]},
-            success: function() {
-              console.log("success");
-            },
-            error: function(xhr,error){
-              console.log(xhr.responseText);
-            }
-          });
-    //      emotion_detection.detectEmotion(this.captures[0]);
-          /*var emodtion_detection = '../assets/emotionDetection.py';
-          var  PythonShell = require('python-shell');
-          var options = {
-              args: this.captures[0]
-          };
-
-          PythonShell.run('../assets/emotionDetection.py', options, function (err, results) {
-              if (err) throw err;
-              // results is an array consisting of messages collected during execution
-              console.log('results: %j', results);
-          });*/
-      }
+  public ngOnInit() {
 
   }
+
+  public ngAfterViewInit() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.video.nativeElement.srcObject = stream;
+        this.video.nativeElement.play();
+      });
+    }
+  }
+
+  public capture() {
+    var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+
+    //  console.log(this.captures[0]);
+
+    $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:5000/post',
+      data: { img: context },
+      success: function () {
+        console.log("success");
+      },
+      error: function (xhr, error) {
+        console.log(xhr.responseText);
+      }
+    });
+    //      emotion_detection.detectEmotion(this.captures[0]);
+    /*var emodtion_detection = '../assets/emotionDetection.py';
+    var  PythonShell = require('python-shell');
+    var options = {
+        args: this.captures[0]
+    };
+
+    PythonShell.run('../assets/emotionDetection.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+    });*/
+  }
+
+}
